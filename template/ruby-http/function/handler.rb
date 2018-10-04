f58e16c96a7b8ede54f685771229272edc538a56
TPei <t.s.peikert@gmail.com>
2018-10-04 09:08:06 +0200

    Ruby indentation fixes
    Signed-off-by: TPei <t.s.peikert@gmail.com>

--- template/ruby-http/function/handler.rb
+++ template/ruby-http/function/handler.rb
@@ -1,8 +1,8 @@
 class Handler
-    def run(body, headers)
-        response_headers = {"content-type": "text/plain"}
-        body = "Hello world from the Ruby template"
+  def run(body, headers)
+    response_headers = {"content-type": "text/plain"}
+    body = "Hello world from the Ruby template"
 
-        return body, response_headers
-    end
+    return body, response_headers
+  end
 end
