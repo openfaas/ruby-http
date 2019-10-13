5d8eae05ab7db2f6631c2262dbf9d8ac6ab87df6
Alexis Reigel <mail@koffeinfrei.org>
2019-10-13 18:01:00 +0200

    add optional response status
    Signed-off-by: Alexis Reigel <mail@koffeinfrei.org>

--- template/ruby-http/function/handler.rb
+++ template/ruby-http/function/handler.rb
@@ -1,8 +1,9 @@
 class Handler
   def run(body, headers)
+    status_code = 200 # Optional status code, defaults to 200
     response_headers = {"content-type": "text/plain"}
     body = "Hello world from the Ruby template"
 
-    return body, response_headers
+    return body, response_headers, status_code
   end
 end
